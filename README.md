6981b3fa4e5d80529652e46eb91b7deb6d12bdd2
Jorge Collazos <jorgecollaz@gmail.com>
2022-04-18 09:24:42 -0500

    add Readme and script

---
+++ README.md
@@ -0,0 +1,47 @@
+# Web Scarping 
+
+Extract the last three blog posts (title, image, and description)
+
+
+## Used By
+
+This project is used by the following companies:
+
+- Gestiona y Aprende S.A.C.
+
+
+## Tech Stack
+
+**Server:** Node, Express
+
+
+## Run Locally
+
+Clone the project
+
+```bash
+  git clone https://github.com/jorgecollazos/web_scraping_blog
+```
+
+Go to the project directory
+
+```bash
+  cd web_scraping_blog
+```
+
+Install dependencies
+
+```bash
+  npm install
+```
+
+Start the server
+
+```bash
+  npm run start
+```
+
+
+## Support
+
+For support, email jorgecollaz@gmail.com.
--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var info = [];
 
 async function init() {
     const $ = await request({
-        uri: 'https://gestionayaprende.com/blog-test/',
+        uri: 'https://gestionayaprende.com/blog/',
         transform: body => cheerio.load(body)
     });
 
@@ -25,12 +25,12 @@ async function init() {
     trimmedString = trimmedString.substr(0, Math.min(trimmedString.length, trimmedString.lastIndexOf(" "))) + " ..."
     info.push({title: second_quote.find('a').html(), img: second_quote.find('.blog_post_image').find('img').attr('src'), description: trimmedString, url: second_quote.find('p').find('a').attr('href')});
 
-    // const third_quote = $('.blog_post_container').next().next();
-    // var dsp = third_quote.find('p').html();
-    // var maxLength = 220;
-    // var trimmedString = dsp.substr(0, maxLength);
-    // trimmedString = trimmedString.substr(0, Math.min(trimmedString.length, trimmedString.lastIndexOf(" "))) + " ..."
-    // info.push({title: third_quote.find('a').html(), img: third_quote.find('.blog_post_image').find('img').attr('src'), description: trimmedString, url: third_quote.find('p').find('a').attr('href')});
+    const third_quote = $('.blog_post_container').next().next();
+    var dsp = third_quote.find('p').html();
+    var maxLength = 220;
+    var trimmedString = dsp.substr(0, maxLength);
+    trimmedString = trimmedString.substr(0, Math.min(trimmedString.length, trimmedString.lastIndexOf(" "))) + " ..."
+    info.push({title: third_quote.find('a').html(), img: third_quote.find('.blog_post_image').find('img').attr('src'), description: trimmedString, url: third_quote.find('p').find('a').attr('href')});
 
     console.log(info);
 
@@ -38,5 +38,60 @@ async function init() {
     fs.writeFileSync('info.json', data);
 }
 
-init(); 
+obj1 = {
+    name: 'https://gestionayaprende.com/blog/author/djesus/',
+    pots: null
+}
+
+obj2 = {
+    name: 'https://gestionayaprende.com/blog/author/mquispe/',
+    pots: null
+}
+obj3 = {
+    name: 'https://gestionayaprende.com/blog/author/dsanchez/',
+    pots: null
+}
+obj4 = {
+    name: 'https://gestionayaprende.com/blog/author/msantos/',
+    pots: null
+}
+
+let hola = [obj1, obj2, obj3, obj4];
+
+
+async function post(url){
+    const $ = await request({
+        uri: url,
+        transform: body => cheerio.load(body)
+    });
+
+    const quote = $('.blog_post_container');
+
+    const post = quote.find('a').html();
+
+    if(post == null){
+        return 1;
+    }
+    else{
+        return 2;
+    }
+}
+
+async function line_break(data){
+    for (var i = 0; i < data.length; i++){
+        data[i].pots = await post(data[i].name);
+    }
+    return data;
+}
+
+async function eject(){
+    hola2 = await line_break(hola);
+    console.log(hola2);
+}
+
+eject();
+
+init();
+
+
 
